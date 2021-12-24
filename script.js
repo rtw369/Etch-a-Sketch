0acd1a79aaeaaa677ee1b33323905e588fd67d54
rtw369 <taewoorim@gmail.com>
2021-12-23 23:15:57 -0800

    Completed Etch-a-Sketch

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-createGrid(16);
+createGrid(16)
+
+const container = document.querySelector('#container');
 
 const button = document.querySelector('#clear');
 button.addEventListener('click', () => {
     let input = window.prompt("How many squares do you want per side for the new grid?");
-    createGrid(input);
+    
+    if(input < 100 && input > 0) {
+        //clear elements
+        const container = document.querySelector('#container');
+        while(container.firstElementChild) {
+            container.firstElementChild.remove();
+        }
+
+        createGrid(input);
+    }
+    else {
+        alert("Number is either too big or too small, please try again.");
+    }
+    
 });
 
 // create grid of square divs
@@ -17,4 +32,12 @@ function createGrid(num){
         gridSquare.style.height = "" + (800 / num) + "px";
         container.appendChild(gridSquare);
     }
+
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        })
+    })
 }
